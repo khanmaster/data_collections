2f3ac0bbb503dd8dca13e9339d090f67e2f8ad38
khanmaster <eashkhan@hotmail.com>
2021-03-23 14:33:30 +0000

    sets in data collection completd with example

--- dictionaries.py
+++ dictionaries.py
@@ -21,18 +21,29 @@ dev_ops_student = {
 # print(dev_ops_student["completed_lesson"])
 # print(dev_ops_student["completed_lessons_names"][1])
 # print(dev_ops_student.keys())
-# print(dev_ops_student.values())
+# # print(dev_ops_student.values())
+#
+# # Add "operators" as a value of completed_lesson_names
+# dev_ops_student["completed_lessons_names"].append("Operators")
+# print(dev_ops_student)
+#
+# # Change the competed lesson from 3 to 4
+# dev_ops_student["completed_lessons"] = 4
+# print(dev_ops_student["completed_lessons"])
+#
+# # Remove the "data type" from completed_lesson_names
+# dev_ops_student["completed_lessons_names"].remove("data types")
+# print(dev_ops_student["completed_lessons_names"])
 
-# Add "operators" as a value of completed_lesson_names
-dev_ops_student["completed_lessons_names"].append("Operators")
-print(dev_ops_student)
+# Sets? The difference is that Sets are unordered
+# Syntax {}
+# Let's create a set
 
-# Change the competed lesson from 3 to 4
-dev_ops_student["completed_lessons"] = 4
-print(dev_ops_student["completed_lessons"])
-
-# Remove the "data type" from completed_lesson_names
-dev_ops_student["completed_lessons_names"].remove("data types")
-print(dev_ops_student["completed_lessons_names"])
-
-# Sets
+car_parts = {"wheels", " windows", "doors"}
+print(car_parts)
+print(type(car_parts))
+car_parts.add("seats")
+print(car_parts)
+car_parts.discard("doors")
+print(car_parts)
+# Forzen sets - home work
